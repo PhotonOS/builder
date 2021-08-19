5b346772cd57e69da1c31caacc8fa26ff7d776be
Robin Röper <rr@linux.com>
2021-08-20 01:52:39 +0200

    Update prepare-shell.sh

--- scripts/x86_64/src/prepare-shell.sh
+++ scripts/x86_64/src/prepare-shell.sh
@@ -23,7 +23,7 @@ PS1='\[\033[01;33m\]\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
 
 # Execute Setup
 FILE=/root/scripts/setup.sh
-if [[ -f "$FILE" ]]; then
-    sh -c $FILE
+if [[ -f "\$FILE" ]]; then
+    sh -c \$FILE
 fi
 EOF
